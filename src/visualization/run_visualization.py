71b4199b9fe1002fa002e306a2267c631ccf6d32
houcine1amraoui <houcine1amraoui@gmail.com>
2026-03-06 11:17:36 +0100

    up

--- src/visualization/run_visualization.py
+++ src/visualization/run_visualization.py
@@ -1,16 +1,10 @@
 import torch
 
-from src.dataset.load_data import load_data
 from src.models.gdn import GDN
-from src.evaluation.load_checkpoint import load_checkpoint
-from src.visualization.plot_learned_graph import plot_learned_graph, get_important_nodes
-
-# 1. Load Dataset
-# df, sensor_columns = load_data("data/sample/BREMaster-sample2.csv")
-# print("Number of sensors:", len(sensor_columns))  # should be 94
+from src.visualization.plot_learned_graph import create_graph_from_embeddings, plot_learned_graph, get_important_nodes, viz_embedding_space
 
 window_size = 10
-# 5. Initialize GDN
+# Initialize GDN
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_arch = GDN(
     number_nodes=94,
@@ -20,5 +14,8 @@ model_arch = GDN(
     heads=1
 ).to(device)
 
-# plot_learned_graph(model_arch)
-get_important_nodes(model_arch)
+# create_graph_from_embeddings(model_arch)
+# plot_learned_graph("graph.pkl")
+# get_important_nodes("graph.pkl")
+viz_embedding_space(model_arch)
+
