1449ea8165d7dc8c4fe2702972a7558f812d8182
houcine1amraoui <houcine1amraoui@gmail.com>
2026-03-14 10:26:06 +0100

    up

--- src/utils/save_utils.py
+++ src/utils/save_utils.py
@@ -1,14 +1,22 @@
 import numpy as np
 import joblib
 import json
+import yaml
 
 def save_processed(train_array, test_array_actor1, test_array_actor2, scaler, sensor_columns):
 
-    np.save("data/processed/train_array.npy", train_array)
-    np.save("data/processed/test_array_actor1.npy", test_array_actor1)
-    np.save("data/processed/test_array_actor2.npy", test_array_actor2)
+    # 1. Set configuration
+    with open("configs/config.yaml") as f:
+        config = yaml.safe_load(f)
 
-    joblib.dump(scaler, "data/processed/scaler.pkl")
+    processed_data_path = config["dataset"]["processed"]
 
-    with open("data/processed/sensors.json", "w") as f:
+
+    np.save(f"{processed_data_path}/train_array.npy", train_array)
+    np.save(f"{processed_data_path}/test_array_actor1.npy", test_array_actor1)
+    np.save(f"{processed_data_path}/test_array_actor2.npy", test_array_actor2)
+
+    joblib.dump(scaler, f"{processed_data_path}scaler.pkl")
+
+    with open(f"{processed_data_path}/sensors.json", "w") as f:
         json.dump(sensor_columns, f)
