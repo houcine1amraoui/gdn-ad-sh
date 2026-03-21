5b30bd2e4c82892b4c9467f1209a57c8fcbcfc6c
houcine1amraoui <houcine1amraoui@gmail.com>
2026-03-21 21:24:50 +0100

    up

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,5 +1,35 @@
 import torch
 from tqdm import tqdm
+
+def train(model, train_loader, optimizer, epochs, exp_dir, device="cpu"):
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        total_loss = 0
+        
+        for x, y in train_loader:
+            x = x.to(device)
+            y = y.to(device)
+            
+            pred = model(x)
+            loss = model.loss(pred, y)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            total_loss += loss.item()
+        
+        print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader):.6f}")
+    
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, f"{exp_dir}/gdn_checkpoint{epoch+1}.pth")
+
+
+import torch
+from tqdm import tqdm
 import os
 
 # def train(model, train_loader, val_loader, optimizer, epochs, exp_dir,
@@ -74,32 +104,3 @@ import os
 #         'model_state_dict': model.state_dict(),
 #         'optimizer_state_dict': optimizer.state_dict(),
 #     }, f"{exp_dir}/last_model.pth")
-
-# import torch
-# from tqdm import tqdm
-
-def train(model, train_loader, optimizer, epochs, exp_dir, device="cpu"):
-    for epoch in tqdm(range(epochs)):
-        model.train()
-        total_loss = 0
-        
-        for x, y in train_loader:
-            x = x.to(device)
-            y = y.to(device)
-            
-            pred = model(x)
-            loss = model.loss(pred, y)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            total_loss += loss.item()
-        
-        print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader):.6f}")
-    
-    torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-    }, f"{exp_dir}/gdn_checkpoint{epoch+1}.pth")
