816bdae483977cc62ce75d0f42cc8b8ed6fd5b3b
houcine1amraoui <houcine1amraoui@gmail.com>
2026-03-25 22:39:56 +0100

    up

--- src/evaluation/load_checkpoint.py
+++ src/evaluation/load_checkpoint.py
@@ -6,7 +6,7 @@ def load_checkpoint(model, path, optimizer):
     device = get_device()
     # if training was done on GPU but evaluation will be on CPU
     # use map_location=torch.device('cpu')
-    checkpoint = torch.load(path, map_location=torch.device('cpu'), weights_only=True)
+    checkpoint = torch.load(path, map_location=torch.device(device), weights_only=True)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     start_epoch = checkpoint['epoch']
