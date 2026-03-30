1d169ed2a0d08160ac3345e9bb69fd147b965112
houcine1amraoui <houcine1amraoui@gmail.com>
2026-03-30 10:18:33 +0100

    pp

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from main_preprocess import main_preprocess
 from main_train import main_train
-from main_evaluation import main_evaluation
 
 def main():
     main_preprocess()
     main_train()
-    main_evaluation()
-
 
 if __name__ == "__main__":
     main()
