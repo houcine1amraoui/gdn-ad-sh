0b9079c826536f83919d324c737b5fcd258f85b9
houcine1amraoui <houcine1amraoui@gmail.com>
2026-04-08 16:50:31 +0100

    up

--- main_evaluation.py
+++ main_evaluation.py
@@ -1,4 +1,5 @@
 import yaml
+import argparse
 
 from src.utils.seed import set_seed
 from src.evaluation.compute_errors import compute_errors
@@ -10,6 +11,15 @@ def main_evaluation():
     with open("configs/config.yaml") as f:
         config = yaml.safe_load(f)
     set_seed(config["seed"])
+
+    # parse CLI args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--project_root_dir", type=str)
+    args = parser.parse_args()
+
+    # override project_root_directory
+    if args.project_root_dir:
+        config["project_root_dir"] = args.project_root_dir
 
     compute_errors(config)
     # scores = compute_scores(config)
