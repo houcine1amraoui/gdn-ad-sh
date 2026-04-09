b76636c63ca660a674afb2dcb71bd67d81833ebd
houcine1amraoui <houcine1amraoui@gmail.com>
2026-04-09 14:20:19 +0100

    up

--- main_evaluation.py
+++ main_evaluation.py
@@ -26,8 +26,8 @@ def main_evaluation():
     # → normalized erros 
     # → scores (aggregated errors per timestamp) 
     # → metrics (point-wise or segment-wise)
-    # compute_errors(config)
-    # compute_scores(config)
+    compute_errors(config)
+    compute_scores(config)
     compute_point_wise_metrics(config)
     compute_segment_wise_metrics(config)
 
